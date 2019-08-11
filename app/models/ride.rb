bdf3eade1023fa80bec5fe9017ce6f452b895cb9
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-11 23:04:22 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,8 +3,8 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-    if self.user.tickets < self.attraction.tickets
-      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
     elsif self.user.height < self.attraction.min_height
       "Sorry. You are not tall enough to ride the #{attraction.name}."
     elsif self.user.tickets < self.attraction.tickets
