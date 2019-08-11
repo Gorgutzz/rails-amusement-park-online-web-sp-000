90c5bfbee344525e2000451b327c251533ecab66
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-11 22:46:47 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,46 +3,20 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-
-    if !enough_tickets_to_ride? && !tall_enough_to_ride?
-      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
-
-    elsif !enough_tickets_to_ride?
-    "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
-
-    elsif !tall_enough_to_ride?
-    "Sorry. You are not tall enough to ride the #{self.attraction.name}."
-
+    user = self.user
+    attraction = self.attraction
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
     else
-      update_user_ticket_post_ride
-      update_user_happiness_post_ride
-      update_user_nausea_post_ride
-      "Thanks for riding the #{self.attraction.name}!"
-
+      user.tickets -= attraction.tickets
+      user.nausea += attraction.nausea_rating
+      user.happiness += attraction.happiness_rating
+      user.save
+      "Thanks for riding the #{attraction.name}!"
     end
   end
-
-  def enough_tickets_to_ride?
-    self.user.tickets > self.attraction.tickets
-  end
-
-  def tall_enough_to_ride?
-    self.user.height > self.attraction.min_height
-  end
-
-  def update_user_ticket_post_ride
-    self.user.tickets -= self.attraction.tickets
-    self.user.save
-  end
-
-  def update_user_happiness_post_ride
-    self.user.happiness += self.attraction.happiness_rating
-    self.user.save
-  end
-
-  def update_user_nausea_post_ride
-    self.user.nausea += self.attraction.nausea_rating
-    self.user.save
-  end
-
 end
