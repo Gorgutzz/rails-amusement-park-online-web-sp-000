60c98c1d362bb655801062efb3df6b7601b75d86
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-11 22:53:24 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,19 +6,14 @@ class Ride < ActiveRecord::Base
 
     if self.user.tickets < self.attraction.tickets
         msg +=" You do not have enough tickets to ride the #{self.attraction.name}."
-    end
-    if self.user.height < self.attraction.min_height
+    elsif self.user.height < self.attraction.min_height
         msg +=" You are not tall enough to ride the #{self.attraction.name}."
-    end
-    if msg != "Sorry."
-        return msg
     else
         self.user.tickets -=self.attraction.tickets
         self.user.nausea += self.attraction.nausea_rating
         self.user.happiness += self.attraction.happiness_rating
         self.user.save
         return nil
-
     end
   end
 
