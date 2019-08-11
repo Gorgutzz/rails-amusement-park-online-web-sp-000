ce26f31a2a391b7afb1f8587450b68889337db41
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-11 22:42:51 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -14,7 +14,7 @@ class AttractionsController < ApplicationController
 
     def create
         @attraction = Attraction.create(attraction_params)
-        redirect_to @attraction
+        redirect_to attraction_path(@attraction)
     end
 
     def edit
