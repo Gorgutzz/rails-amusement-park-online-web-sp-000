70d8b5622eb4b6e7914263e8c829b8eb0aa9a2ea
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-11 22:46:09 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     if logged_in?
       @user = User.find(params[:id])
     else
-      redirect_to root_path
+      redirect_to '/'
     end
   end
 
