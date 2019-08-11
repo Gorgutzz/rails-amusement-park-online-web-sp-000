1b5e8c67d391e289166a6e60ae5c49f6caf5fb15
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-11 23:01:22 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,11 +4,11 @@ class Ride < ActiveRecord::Base
 
   def take_ride
     if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
-      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+      "You do not have enough tickets to ride the #{attraction.name} and are not tall enough to ride the #{attraction.name}."
     elsif self.user.tickets < self.attraction.tickets
-      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+      "You do not have enough tickets to ride the #{attraction.name}."
     elsif self.user.height < self.attraction.min_height
-      "Sorry. You are not tall enough to ride the #{attraction.name}."
+      "You are not tall enough to ride the #{attraction.name}."
     else
       self.user.tickets -= self.attraction.tickets
       self.user.nausea += self.attraction.nausea_rating
